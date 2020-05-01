779178c54f0f79809da02c6433e80788c2b479ea
Parth Doshi <studentparth18@gmail.com>
2020-05-01 16:46:44 +0530

    modify project urls

--- gallery/gallery/urls.py
+++ gallery/gallery/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from django.contrib import admin
-from django.urls import path
+# admin.autodiscover()
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = patterns('',
+        (r'^app/', include('app.urls')),
+        (r'^$', 'app.views.index'),
+        (r'^admin/', include(admin.site.urls)),
+
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
