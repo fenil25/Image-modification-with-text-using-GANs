6c539f47d9156d4c148d1e60e82c035a7ea7841e
Parth Doshi <studentparth18@gmail.com>
2020-05-01 16:12:23 +0530

    add edit model

--- gallery/app/models.py
+++ gallery/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+def file_directory_path(instance, file_name):
+	# Returns a file path in the form MEDIA_ROOT/<type>/<num_id>/
+
+class Edit(models.Model):
+	num_id = models.IntegerField()
+	dataset = models.CharField()  # flower/bird/fashion
+	original = models.FileField(upload_to = '{0}/{1}/original.jpg'.format(self.dataset, self.num_id))
+	desc = models.TextField()
+	result = models.FileField(upload_to = '{0}/{1}/result.jpg'.format(self.dataset, self.num_id))
